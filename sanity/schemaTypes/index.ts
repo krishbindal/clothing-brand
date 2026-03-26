0f48373c9e52e320e7a1f2871791abe4688e13c6
YourName <your_email@example.com>
2026-03-26 21:08:18 +0530

    chore: remove previous Sanity files to separate CMS

--- sanity/schemaTypes/index.ts
+++
@@ -1,5 +0,0 @@
-import { collectionType } from './collection'
-import { homepageType } from './homepage'
-import { productType } from './product'
-
-export const schemaTypes = [productType, collectionType, homepageType]
